f2dcacfbee5afbee243c31e02d5fea9cf26b62c3
andrehebra <andrehebra@gmail.com>
2022-02-18 16:54:31 -0500

    created keyboard js file

---
+++ keyboard.js
@@ -0,0 +1,34 @@
+const topRow = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+];
+
+const middleRow = [
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+];
+
+const bottomRow = [
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+];
