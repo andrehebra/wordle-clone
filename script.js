b468f3b8cfa483de964ee5b170a9b6021f588159
andrehebra <andrehebra@gmail.com>
2022-02-18 12:33:01 -0500

    added comments to js

--- script.js
+++ script.js
@@ -1,55 +1,77 @@
+//number of word guesses allowed
 const numberOfAttempts = 6;
+//length of the words that can be guessed
 const wordLength = 5;
 
+//finds the main grid in index.html
 const mainGrid = document.getElementById('main-grid');
 
-
+//adding a grid of elements to mainGrid
 for (i = 1; i <= numberOfAttempts; i++) {
+
+    //creating row and adding attributes to it
     let row = document.createElement('div');
     row.className = 'row';
     row.id = 'row-' + i;
     row.setAttribute('played', 'false');
 
+    //adding the individual letters to each row
     for (j = 1; j <= wordLength; j++) {
+
+        //creating letters and adding attributes
         let letterBox = document.createElement('div');
         letterBox.className = 'letter-box';
         letterBox.id = 'row-' + i + '-letter-' + j;
         letterBox.setAttribute('played', 'false');
 
+        //set default dext to nothing
         letterBox.textContent = '';
 
+        //add the letterbox to each row
         row.appendChild(letterBox);
     }
     
+    //adding each row onto the main-grid
     mainGrid.appendChild(row);
 }
 
+//event listener to catch each keystroke
 window.addEventListener('keydown', function (e) {
-    console.log(e);
 
+    //change keys to uppercase
     console.log(e.key.toUpperCase());
 
+    //check to see if keys pressed are from alphabet
+
+    //add keystroke
     play(e.key.toUpperCase())
 
+    //check if backspace
+
+    //delete previous guess
 });
 
+//add keystroke
 function play(letter) {
+    //check if current row has been played
     for (i = 1; i <= numberOfAttempts; i++) {
         if (document.getElementById('row-' + i).getAttribute('played') == 'false') {
             
+            //check if letters have already been put in row
             for (j = 1; j <= wordLength; j++) {
                 const currentLetter = document.getElementById('row-' + i + '-letter-' + j);
+                //if letter box has not been played yet, add letter there
                 if (currentLetter.getAttribute('played') == 'false') {
                     currentLetter.innerText = letter;
                     currentLetter.setAttribute('played', 'true');
                     return;
                 }
             }
-            
+            //return once the letter has been added
             return;
 
         } else {
-            console.log('didnotwork...');
+            //console.log('didnotwork...');
         }
     }
 }
